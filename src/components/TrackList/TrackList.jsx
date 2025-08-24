57b484bf81a90f3b69d3071fa0cde55ab97e3b11
abdullahh545 <abdullahshaf49@gmail.com>
2025-08-24 22:42:02 +0300

    Initial commit with my changes

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,42 +1,51 @@
-import { useEffect, useState } from "react"
-import { deleteTrack, getAllTracks } from "../../../lib/api"
-import "./TrackList.css"
-const TrackList = ({tracks, setTracks,setEditTrack, setCurrentlyPlaying}) => {
+import { useEffect } from "react";
+import { deleteTrack, getAllTracks } from "../../../lib/api";
+import "./TrackList.css";
 
-    const getTracks = async ()=>{
-        const t = await getAllTracks()
-        setTracks(t)
-    }
-    const removeTrack = async (trackId)=>{
-        await deleteTrack(trackId)
-        getTracks()
-    }
-    const updateTrack = async (track)=>{ 
-        setEditTrack(track)
-    }
+const TrackList = ({
+  tracks,
+  setTracks,
+  setEditTrack,
+  setCurrentlyPlaying,
+}) => {
+  const getTracks = async () => {
+    const all = await getAllTracks();
+    setTracks(Array.isArray(all) ? all : []);
+  };
 
-    useEffect(()=>{
-        getTracks()
-    }, [])
-    return (
-        <div className="trackList">
-            {tracks.length ? tracks.map((track, index)=>{
-                return <div key={index} className="track">
-                    <p>{track.title} by {track.artist}</p>
-                    <button onClick={()=>{
-                        setCurrentlyPlaying(track)
-                    }}>Play</button>
-                    <button onClick={()=>{
-                        updateTrack(track)
-                    }}>Edit</button>
-                    <button onClick={()=>{
-                        removeTrack(track._id)
-                    }}>Delete</button>
-                </div>
-            }) : <h1>Loading</h1> }
-            {}
-        </div>
-    )
-}
+  const removeTrack = async (trackId) => {
+    await deleteTrack(trackId);
+    getTracks();
+  };
 
-export default TrackList
+  const updateTrack = (track) => {
+    setEditTrack(track);
+  };
+
+  useEffect(() => {
+    getTracks();
+  }, []);
+
+  if (!Array.isArray(tracks)) return <h1>Loading...</h1>;
+
+  return (
+    <div className="trackList">
+      {tracks.length > 0 ? (
+        tracks.map((track, index) => (
+          <div key={index} className="track">
+            <p>
+              {track.title} by {track.artist}
+            </p>
+            <button onClick={() => setCurrentlyPlaying(track)}>Play</button>
+            <button onClick={() => updateTrack(track)}>Edit</button>
+            <button onClick={() => removeTrack(track._id)}>Delete</button>
+          </div>
+        ))
+      ) : (
+        <h1>Loading</h1>
+      )}
+    </div>
+  );
+};
+
+export default TrackList;
